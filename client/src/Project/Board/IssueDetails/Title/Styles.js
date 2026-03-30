751cc37e57b0ec63ae0433831a7346adcd73105a
chihaos1 <73306413+chihaos1@users.noreply.github.com>
2026-03-30 18:11:18 -0400

    Agent Arena: Update client/src/Project/Board/IssueDetails/Title/Styles.js

--- client/src/Project/Board/IssueDetails/Title/Styles.js
+++ client/src/Project/Board/IssueDetails/Title/Styles.js
@@ -3,30 +3,52 @@ import styled from 'styled-components';
 import { color, font } from 'shared/utils/styles';
 import { Textarea } from 'shared/components';
 
+export const Title = styled.div`
+  position: relative;
+  padding: 21px 0 6px;
+`;
+
 export const TitleTextarea = styled(Textarea)`
-  margin: 18px 0 0 -8px;
-  height: 44px;
-  width: 100%;
-  textarea {
-    padding: 7px 7px 8px;
-    line-height: 1.28;
-    border: none;
-    resize: none;
-    background: #fff;
-    border: 1px solid transparent;
-    box-shadow: 0 0 0 1px transparent;
-    transition: background 0.1s;
-    ${font.size(24)}
-    ${font.medium}
-    &:hover:not(:focus) {
-      background: ${color.backgroundLight};
-    }
+  min-height: 44px;
+  margin-bottom: 0;
+  padding: 8px 0;
+  border: none;
+  color: ${color.textDarkest};
+  font-weight: ${font.medium};
+  font-size: 24px;
+  transition: background 0.1s;
+  ${props => props.isOverLimit && `
+    color: ${color.danger};
+    background-color: ${color.backgroundLightest};
+  `}
+  &:focus {
+    background: ${color.backgroundLightest};
   }
 `;
 
-export const ErrorText = styled.div`
-  padding-top: 4px;
-  color: ${color.danger};
-  ${font.size(13)}
-  ${font.medium}
+export const CharacterCounter = styled.div`
+  position: absolute;
+  right: 0;
+  top: 4px;
+  font-size: 12px;
+  font-weight: ${font.medium};
+  color: ${props => {
+    if (props.isOverLimit) return color.danger;
+    if (props.isNearLimit) return color.warning;
+    return color.textMedium;
+  }};
+  background: ${color.backgroundLightest};
+  padding: 4px 8px;
+  border-radius: 3px;
+  border: 1px solid ${props => {
+    if (props.isOverLimit) return color.danger;
+    if (props.isNearLimit) return color.warning;
+    return color.borderLightest;
+  }};
+  z-index: 1;
+  
+  span {
+    color: ${color.danger};
+    font-weight: ${font.bold};
+  }
 `;
