ad2a261915b9871f99b788369d2c890097b364cd
chihaos1 <73306413+chihaos1@users.noreply.github.com>
2026-04-04 15:28:38 -0400

    Agent Arena: Update client/src/Project/Board/IssueDetails/Title/Styles.js

--- client/src/Project/Board/IssueDetails/Title/Styles.js
+++ client/src/Project/Board/IssueDetails/Title/Styles.js
@@ -3,30 +3,29 @@ import styled from 'styled-components';
 import { color, font } from 'shared/utils/styles';
 import { Textarea } from 'shared/components';
 
-export const TitleTextarea = styled(Textarea)`
-  margin: 18px 0 0 -8px;
-  height: 44px;
-  width: 100%;
-  textarea {
-    padding: 7px 7px 8px;
-    line-height: 1.28;
-    border: none;
-    resize: none;
-    background: #fff;
-    border: 1px solid transparent;
-    box-shadow: 0 0 0 1px transparent;
-    transition: background 0.1s;
-    ${font.size(24)}
-    ${font.medium}
-    &:hover:not(:focus) {
-      background: ${color.backgroundLight};
-    }
+export const Title = styled.div`
+  padding: 3px 8px 3px 12px;
+  border-radius: 3px;
+  transition: background 0.1s;
+  ${font.size(24)}
+  ${font.medium}
+  &:hover {
+    background: ${color.backgroundLight};
   }
 `;
 
-export const ErrorText = styled.div`
-  padding-top: 4px;
-  color: ${color.danger};
-  ${font.size(13)}
+export const TitleTextarea = styled(Textarea)`
+  ${font.size(24)}
   ${font.medium}
+  border: none;
+  padding: 0;
+  color: ${color.textDarkest};
+`;
+
+export const CharacterCounter = styled.div`
+  font-size: 12px;
+  color: ${props => props.isNearLimit ? color.danger : color.textMedium};
+  text-align: right;
+  margin-top: 4px;
+  font-weight: ${props => props.isNearLimit ? '600' : '400'};
 `;
