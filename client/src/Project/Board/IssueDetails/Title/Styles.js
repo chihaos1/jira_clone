2fcdeb0130e10ecdbc6715f421120cfca564ba01
chihaos1 <73306413+chihaos1@users.noreply.github.com>
2026-04-03 22:59:23 -0400

    Agent Arena: Update client/src/Project/Board/IssueDetails/Title/Styles.js

--- client/src/Project/Board/IssueDetails/Title/Styles.js
+++ client/src/Project/Board/IssueDetails/Title/Styles.js
@@ -1,32 +1,47 @@
 import styled from 'styled-components';
 
 import { color, font } from 'shared/utils/styles';
-import { Textarea } from 'shared/components';
 
-export const TitleTextarea = styled(Textarea)`
-  margin: 18px 0 0 -8px;
-  height: 44px;
-  width: 100%;
-  textarea {
-    padding: 7px 7px 8px;
-    line-height: 1.28;
-    border: none;
-    resize: none;
-    background: #fff;
-    border: 1px solid transparent;
-    box-shadow: 0 0 0 1px transparent;
-    transition: background 0.1s;
-    ${font.size(24)}
-    ${font.medium}
-    &:hover:not(:focus) {
-      background: ${color.backgroundLight};
-    }
+export const TextEditedContent = styled.div`
+  padding: 8px 12px;
+  border-radius: 3px;
+  transition: background 0.1s;
+  ${font.size(24)}
+  ${font.medium}
+  &:hover {
+    background: ${color.backgroundLight};
   }
 `;
 
-export const ErrorText = styled.div`
-  padding-top: 4px;
-  color: ${color.danger};
-  ${font.size(13)}
+export const TextEditor = styled.input`
+  ${font.size(24)}
   ${font.medium}
+  width: 100%;
+  padding: 8px 12px;
+  border: 2px solid ${color.borderLightest};
+  border-radius: 3px;
+  background: ${color.backgroundLightest};
+  transition: border 0.1s;
+  &:focus {
+    outline: none;
+    border: 2px solid ${color.borderInputFocus};
+    background: #fff;
+  }
+`;
+
+export const Actions = styled.div`
+  display: flex;
+  margin-top: 8px;
+  padding-top: 12px;
+  button {
+    margin-right: 6px;
+  }
+`;
+
+export const CharacterCounter = styled.div`
+  font-size: 12px;
+  color: ${props => props.isNearLimit ? color.danger : color.textMedium};
+  text-align: right;
+  margin-top: 4px;
+  font-weight: ${props => props.isNearLimit ? '600' : '400'};
 `;
