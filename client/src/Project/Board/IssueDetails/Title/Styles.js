025a657b217c93812da312d67eaa8c4ac3411f95
chihaos1 <73306413+chihaos1@users.noreply.github.com>
2026-06-24 11:19:05 -0400

    Agent Arena: Update client/src/Project/Board/IssueDetails/Title/Styles.js

--- client/src/Project/Board/IssueDetails/Title/Styles.js
+++ client/src/Project/Board/IssueDetails/Title/Styles.js
@@ -1,32 +1,37 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { color, font, mixin } from 'shared/utils/styles';
 
-import { color, font } from 'shared/utils/styles';
-import { Textarea } from 'shared/components';
-
-export const TitleTextarea = styled(Textarea)`
-  margin: 18px 0 0 -8px;
-  height: 44px;
-  width: 100%;
-  textarea {
-    padding: 7px 7px 8px;
-    line-height: 1.28;
-    border: none;
-    resize: none;
-    background: #fff;
-    border: 1px solid transparent;
-    box-shadow: 0 0 0 1px transparent;
-    transition: background 0.1s;
-    ${font.size(24)}
-    ${font.medium}
-    &:hover:not(:focus) {
-      background: ${color.backgroundLight};
-    }
-  }
+export const TitleContainer = styled.div`
+  position: relative;
+  margin-bottom: 5px;
 `;
 
-export const ErrorText = styled.div`
-  padding-top: 4px;
-  color: ${color.danger};
-  ${font.size(13)}
-  ${font.medium}
+export const Textarea = styled.textarea`
+  width: 100%;
+  padding: 7px 7px;
+  border-radius: 3px;
+  border: 1px solid transparent;
+  background: #fff;
+  ${font.size(24)}
+  font-weight: bold;
+  line-height: 1.4;
+  resize: none;
+  overflow: hidden;
+  transition: background 0.1s;
+  &:focus,
+  &:hover {
+    background: ${color.backgroundLight};
+    outline: none;
+  }
+  ${mixin.customScrollbar()}
+`;
+
+export const CharacterCounter = styled.div`
+  text-align: right;
+  ${font.size(12)}
+  color: ${({ isNearLimit }) =>
+    isNearLimit ? color.danger : color.textMedium};
+  margin-top: 2px;
+  padding-right: 2px;
+  transition: color 0.2s;
 `;
